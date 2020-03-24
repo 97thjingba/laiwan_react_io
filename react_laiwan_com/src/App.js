630cf620e8abae0b580a4a3c1634b3a9775d1e0c
97thjingba <1079937489@qq.com>
2020-03-24 18:26:35 +0800

    update

--- react_laiwan_com/src/App.js
+++ react_laiwan_com/src/App.js
@@ -2,6 +2,9 @@
 import React, { Component } from 'react';
 import BackgroundImage from 'react-background-image';
 import './view/style/home.css';
+import {
+    BrowserRouter as Router,
+} from 'react-router-dom';
 import bigPhone from './source/img-phone.png';
 import smallPhone from './source/small-phone.png';
 import logo from './source/logo.png';
@@ -21,28 +24,29 @@ export default class App extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="navigation_bar">
-                    <div className="row_center">
-                        <img src={logo} className="logo" />
-                        <p className="title">来玩</p>
+            <Router>
+                <div className="container">
+                    <div className="navigation_bar">
+                        <div className="row_center">
+                            <img src={logo} className="logo" />
+                            <p className="title">来玩</p>
+                        </div>
+                        <div className="row_center">
+                            <a href="/"><p className="subtitle">首页</p></a>
+                            <a href="/"><p className="subtitle">德州术语表</p></a>
+                        </div>
                     </div>
-                    <div className="row_center">
-                        <a href="/"><p className="subtitle">首页</p></a>
-                        <a href="/"><p className="subtitle">德州术语表</p></a>
+                    <div className="content">
+                        <div className="phone_content">
+                            <BackgroundImage
+                                placeholder={smallPhone}
+                                src={bigPhone}
+                                className="phone"
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="content">
-                    <div className="phone_content">
-                        <BackgroundImage
-                            placeholder={smallPhone}
-                            src={bigPhone}
-                            className="phone"
-                        />
-                    </div>
-                </div>
-            </div>
-            // 暂时还是不把这放在背景里了.还是直接在body里面放...这个用来加载轮播图吧.不然文档流不知道如何设置
+            </Router>
         );
     }
 }
--- react_laiwan_com/src/index.js
+++ react_laiwan_com/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// 主要是用于在生产环境中为用户在本地创建一个service worker 来缓存资源到本地，提升应用的访问速度
+import registerServiceWorker from './utils/registerServiceWorker';
 
 const home = document.getElementById('laiwan');
 
